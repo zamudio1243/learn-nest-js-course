323b35dbc304edba776d49921ba4b1a63702f8b6
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2026-03-22 22:44:47 -0600

    ✨ chapter 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import sql from "./db";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -104,4 +106,23 @@ export async function deleteInvoice(id: string) {
   throw new Error("Failed to Delete Invoice");
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -140,6 +140,7 @@ export default function EditInvoiceForm({
                     {error}
                   </p>
                 ))}
+            </div>
           </div>
         </fieldset>
       </div>
